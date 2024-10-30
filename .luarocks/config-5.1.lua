2c034a4dcf245d01fadada81eca159458d45a881
Komo <71205197+mbekkomo@users.noreply.github.com>
2024-10-31 02:47:28 +0800

    push

---
+++ .luarocks/config-5.1.lua
@@ -0,0 +1,6 @@
+variables = {
+   LUA = "/nix/store/argnfqfaxji0bfxfdn0ccm4c2ym9kdm0-luajit-2.1.1713773202/bin/luajit",
+   LUA_BINDIR = "/nix/store/argnfqfaxji0bfxfdn0ccm4c2ym9kdm0-luajit-2.1.1713773202/bin",
+   LUA_DIR = "/nix/store/argnfqfaxji0bfxfdn0ccm4c2ym9kdm0-luajit-2.1.1713773202",
+   LUA_INCDIR = "/nix/store/argnfqfaxji0bfxfdn0ccm4c2ym9kdm0-luajit-2.1.1713773202/include/luajit-2.1"
+}
--- Makefile
+++
@@ -1,9 +0,0 @@
-all: static/js/fengari-web.js
-
-serve: all
-	lapis server production
-
-static/js/fengari-web.js:
-	curl -L https://github.com/fengari-lua/fengari-web/releases/download/v0.1.4/fengari-web.js -o $@
-
-.PHONY: all serve
